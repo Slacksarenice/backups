e20a4a007bc9e102296bab3a196c386b76d65be8
slacksarenice <sethmmillr@gmail.com>
2021-03-27 22:44:26 -0400

    Argument Functionality

--- backup.py
+++ backup.py
@@ -1,5 +1,6 @@
 #! /usr/bin/env python3.6
 
+import argparse
 import os
 import gnupg
 from datetime import datetime
@@ -7,18 +8,11 @@ from pathlib import Path, PurePath
 from py7zr import FILTER_LZMA2, SevenZipFile
 
 
-
-BACKUPDIR = '/media/slacks/BackupStuff' #Replace this with your backup location
-BACKUPAMOUNT = 5
-filetobackup = Path().home()
-directorytobackto = Path(BACKUPDIR)
-
-
-def valdir():
+def valdir(*args):
     try:
-        assert filetobackup.exists()
-        directorytobackto.mkdir(parents=True, exist_ok=True)
-        existingbackups = [ x for x in directorytobackto.iterdir()
+        assert args[0].filetobackup[0].exists()
+        args[0].backuplocation[0].mkdir(parents=True, exist_ok=True)
+        existingbackups = [ x for x in args[0].backuplocation[0].iterdir()
         if x.is_file() and x.suffix == '.7z' and x.name.startswith('backup-')]
 
     except OSError as e:
@@ -27,46 +21,45 @@ def valdir():
     return existingbackups
 
 
-def enformaxbackups(backups=valdir()):
+def enformaxbackups(backups,*args):
     oldtonew = list(sorted(backups, key=lambda f: f.name))
 
-    while len(oldtonew) >= BACKUPAMOUNT:
+    while len(oldtonew) >= args[0].instances:
         backupdel = oldtonew.pop(0)
         backupdel.unlink()
     
     return True
 
 
-def create7z():
+def create7z(*args):
     filters = [{'id': FILTER_LZMA2}]
-    backupname = f'backup-{datetime.now().strftime("%Y%m%d%H%M%S")}-{filetobackup.name}.7z'
-    zipfile = SevenZipFile(str(directorytobackto / backupname), mode='w', filters=filters)
+    backupname = f'backup-{datetime.now().strftime("%Y%m%d%H%M%S")}-{args[0].filetobackup[0].name}.7z'
+    zipfile = SevenZipFile(str(args[0].backuplocation[0] / backupname), mode='w', filters=filters)
     try:
-        if filetobackup.is_file():
-            zipfile.write(filetobackup.absolute(), arcname=filetobackup.name)
+        if args[0].filetobackup[0].is_file():
+            zipfile.write(args[0].filetobackup[0].absolute(), arcname=args[0].filetobackup[0].name)
             
-        elif filetobackup.is_dir():
-            for file in filetobackup.rglob('*'):
+        elif args[0].filetobackup[0].is_dir():
+            for file in args[0].filetobackup[0].rglob('*'):
                 if file.is_file():
-                    zipfile.write(file.absolute(),arcname=str(file.relative_to(filetobackup)))
+                    zipfile.write(file.absolute(),arcname=str(file.relative_to(args[0].filetobackup[0])))
 
     except Exception as e:
         print(e)
 
     zipfile.close()
-    encryptfile(backupname)
+    return backupname
 
 
-def encryptfile(filename):
-    fingerprint = 'DE5AE8073361E11B6ED3B629014E4BA24373BA83' #Replace this with your key's fingerprint unless you want my encryption key on your files
+def encryptfile(filename,*args):
     try:
         gpg = gnupg.GPG(gpgbinary='gpg2')
-        with open(f'{directorytobackto.resolve()}/{filename}', "r+b") as f:
-            encyptedfiles = gpg.encrypt_file(f, recipients=fingerprint, always_trust=True, sign=fingerprint)
+        with open(f'{args[0].backuplocation[0].resolve()}/{filename}', "r+b") as f:
+            encyptedfiles = gpg.encrypt_file(f, recipients=args[0].fingerprint[0], always_trust=True, sign=args[0].fingerprint[0])
             f.seek(0)
             f.write(encyptedfiles.data)
             f.truncate()
-        os.rename(f'{directorytobackto.resolve()}/{filename}',f'{directorytobackto.resolve()}/{filename}.gpg')
+        os.rename(f'{args[0].backuplocation[0].resolve()}/{filename}',f'{args[0].backuplocation[0].resolve()}/{filename}.gpg')
 
 
     except ValueError as e:
@@ -74,14 +67,31 @@ def encryptfile(filename):
         pass
 
 
-
-
-
-
 if __name__ == '__main__':
-    BACKUPDIR = '/media/slacks/BackupStuff' #Replace this with your backup location
-    BACKUPAMOUNT = 5
-    filetobackup = Path().home()
-    directorytobackto = Path(BACKUPDIR)
-    enformaxbackups()
-    create7z()
+    try:
+        parser = argparse.ArgumentParser(description="Backup Files and encrypt them with gpg")
+
+        parser.add_argument('filetobackup', type=Path, nargs=1, help='Path of the file to backup')
+        parser.add_argument('backuplocation', type=Path, nargs=1, help='Path of the location to save backup')
+        parser.add_argument('--instances','-i',nargs=1, default=5, help='Number of backupfiles to check for and delete oldest backups over amount')
+        
+        subparser = parser.add_subparsers(dest = 'commands', help="Optional arguments")
+        encryptionparser = subparser.add_parser('encrypt',help='Encrypt data options')
+        encryptionparser.add_argument('--fingerprint', '-f',nargs=1,help='Fingerprint to encrypt with', required=True)
+
+        args = parser.parse_args()
+        enformaxbackups(valdir(args),args)
+        filename = create7z(args)
+        if args.commands == 'encrypt':
+            encryptfile(filename, args)
+            
+
+    except BaseException as e:
+        print(f'Error! {e.args}')
+
+    #BACKUPDIR = '/media/slacks/BackupStuff' #Replace this with your backup location
+    #BACKUPAMOUNT = 5
+    #filetobackup = Path().home()
+    #backuplocation = Path(BACKUPDIR)
+    #enformaxbackups()
+    #create7z()
