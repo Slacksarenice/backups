c481194544867349e760e07d938c6587bb35db3f
slacksarenice <sethmmillr@gmail.com>
2021-03-28 01:49:47 -0400

    Clean Up Arguments

--- backup.py
+++ backup.py
@@ -6,13 +6,14 @@ import gnupg
 from datetime import datetime
 from pathlib import Path, PurePath
 from py7zr import FILTER_LZMA2, SevenZipFile
+from sys import argv
 
 
 def valdir(*args):
     try:
-        assert args[0].FILETOBACKUP[0].exists()
-        args[0].BACKUPLOCATION[0].mkdir(parents=True, exist_ok=True)
-        existingbackups = [ x for x in args[0].BACKUPLOCATION[0].iterdir()
+        assert args[0].FILETOBACKUP.exists()
+        args[0].BACKUPLOCATION.mkdir(parents=True, exist_ok=True)
+        existingbackups = [ x for x in args[0].BACKUPLOCATION.iterdir()
         if x.is_file() and x.suffix == '.7z' and x.name.startswith('backup-')]
 
     except OSError as e:
@@ -33,16 +34,16 @@ def enformaxbackups(backups,*args):
 
 def create7z(*args):
     filters = [{'id': FILTER_LZMA2}]
-    backupname = f'backup-{datetime.now().strftime("%Y%m%d%H%M%S")}-{args[0].FILETOBACKUP[0].name}.7z'
-    zipfile = SevenZipFile(str(args[0].BACKUPLOCATION[0] / backupname), mode='w', filters=filters)
+    backupname = f'backup-{datetime.now().strftime("%Y%m%d%H%M%S")}-{args[0].FILETOBACKUP.name}.7z'
+    zipfile = SevenZipFile(str(args[0].BACKUPLOCATION / backupname), mode='w', filters=filters)
     try:
-        if args[0].FILETOBACKUP[0].is_file():
-            zipfile.write(args[0].FILETOBACKUP[0].absolute(), arcname=args[0].FILETOBACKUP[0].name)
+        if args[0].FILETOBACKUP.is_file():
+            zipfile.write(args[0].FILETOBACKUP.absolute(), arcname=args[0].FILETOBACKUP.name)
             
-        elif args[0].FILETOBACKUP[0].is_dir():
-            for file in args[0].FILETOBACKUP[0].rglob('*'):
+        elif args[0].FILETOBACKUP.is_dir():
+            for file in args[0].FILETOBACKUP.rglob('*'):
                 if file.is_file():
-                    zipfile.write(file.absolute(),arcname=str(file.relative_to(args[0].FILETOBACKUP[0])))
+                    zipfile.write(file.absolute(),arcname=str(file.relative_to(args[0].FILETOBACKUP)))
 
     except Exception as e:
         print(e)
@@ -54,12 +55,12 @@ def create7z(*args):
 def encryptfile(filename,*args):
     try:
         gpg = gnupg.GPG(gpgbinary='gpg2')
-        with open(f'{args[0].BACKUPLOCATION[0].resolve()}/{filename}', "r+b") as f:
-            encyptedfiles = gpg.encrypt_file(f, recipients=args[0].fingerprint[0], always_trust=True, sign=args[0].fingerprint[0])
+        with open(f'{args[0].BACKUPLOCATION.resolve()}/{filename}', "r+b") as f:
+            encyptedfiles = gpg.encrypt_file(f, recipients=args[0].fingerprint, always_trust=True, sign=args[0].fingerprint)
             f.seek(0)
             f.write(encyptedfiles.data)
             f.truncate()
-        os.rename(f'{args[0].BACKUPLOCATION[0].resolve()}/{filename}',f'{args[0].BACKUPLOCATION[0].resolve()}/{filename}.gpg')
+        os.rename(f'{args[0].BACKUPLOCATION.resolve()}/{filename}',f'{args[0].BACKUPLOCATION.resolve()}/{filename}.gpg')
 
 
     except ValueError as e:
@@ -70,13 +71,12 @@ def encryptfile(filename,*args):
 if __name__ == '__main__':
     try:
         parser = argparse.ArgumentParser(description="Backup Files and encrypt them with gpg", usage='%(prog)s [options] [Backup File or Directory] [Backup Location]')
-        optionalarguments = parser.add_argument_group('optionalarguments', 'List of Optional Arguments')
-        primaryuse = parser.add_argument_group('Backup Use')
-        primaryuse.add_argument('FILETOBACKUP', type=Path, nargs=1, help='Path of the file to backup')
-        primaryuse.add_argument('BACKUPLOCATION', type=Path, nargs=1, help='Path of the location to save backup')
-        optionalarguments.add_argument('-e', '--encrypt', action='store_true')
-        optionalarguments.add_argument('-f', '--fingerprint',nargs=1,help='Fingerprint to encrypt with', required=True if parser.parse_args().encrypt else False)
-        optionalarguments.add_argument('-i','--instances',nargs=1, default=5, help='Number of backupfiles to check for and delete oldest backups over amount')
+        parser.add_argument('FILETOBACKUP', type=Path, help='Path of the file to backup')
+        parser.add_argument('BACKUPLOCATION', type=Path, help='Path of the location to save backup')
+        parser.add_argument('-e', '--encrypt', action='store_true')
+        parser.add_argument('-f', '--fingerprint', type=str,help='Fingerprint to encrypt with', required='-f' in argv or '--fingerprint' in argv)
+        parser.add_argument('-i','--instances',nargs=1, type=int, default=5, help='Number of backupfiles to check for and delete oldest backups over amount')
+        
                
         args = parser.parse_args()
         enformaxbackups(valdir(args),args)
